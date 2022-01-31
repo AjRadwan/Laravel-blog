3d1e130b3b6a6ae4baf57d0ebb6098c0a462e820
Anowar <1hafizajr@gmail.com>
2022-01-31 13:49:09 +0600

    added layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,51 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>About Anowar - Anowar Blog</title>
-  <!-- Css -->
-  <link rel="stylesheet" href="{{asset('css/style.css')}}" />
-  <!-- Font awesome -->
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-</head>
-
-<body>
-  <div id="wrapper">
-    <!-- sidebar -->
-    <div class="sidebar">
-      <span class="closeButton">&times;</span>
-      <p class="brand-title"><a href="">Anowar Blog</a></p>
-
-      <div class="side-links">
-        <ul>
-          <li><a class="active" href="/">Home</a></li>
-          <li><a href="blog">Blog</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="contact">Contact</a></li>
-        </ul>
-      </div>
-      <!-- sidebar footer -->
-      <footer class="sidebar-footer">
-        <div>
-          <a href=""><i class="fab fa-facebook-f"></i></a>
-          <a href=""><i class="fab fa-instagram"></i></a>
-          <a href=""><i class="fab fa-twitter"></i></a>
-        </div>
-
-        <small>&copy 2021 Anowar Blog</small>
-      </footer>
-    </div>
-    <!-- Menu Button -->
-    <div class="menuButton">
-      <div class="bar"></div>
-      <div class="bar"></div>
-      <div class="bar"></div>
-    </div>
-    <!-- main -->
+@extends('layout')
+ 
+    @section('main')
     <main class="container">
       <section class="single-blog-post">
         <h1>About Me</h1>
@@ -74,32 +29,5 @@
       </section>
     </main>
 
-    <!-- footer -->
-    <footer class="main-footer">
-      <div>
-        <a href=""><i class="fab fa-facebook-f"></i></a>
-        <a href=""><i class="fab fa-instagram"></i></a>
-        <a href=""><i class="fab fa-twitter"></i></a>
-      </div>
-      <small>&copy 2021 Anowar Blog</small>
-    </footer>
-  </div>
-
-  <!-- Click events to menu and close buttons using javaascript-->
-  <script>
-    document
-      .querySelector(".menuButton")
-      .addEventListener("click", function () {
-        document.querySelector(".sidebar").style.width = "100%";
-        document.querySelector(".sidebar").style.zIndex = "5";
-      });
-
-    document
-      .querySelector(".closeButton")
-      .addEventListener("click", function () {
-        document.querySelector(".sidebar").style.width = "0";
-      });
-  </script>
-</body>
-
-</html>
+    @endsection
+    
